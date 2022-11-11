15402f553cc161ce8b78520c91a3b7d8cf88a728
thephotogenicbug <navinkumarachari@gmail.com>
2022-11-11 15:46:14 +0530

    create ViewModel

--- app/src/main/java/com/naveen/notesappmvvm/View/MainActivity.kt
+++ app/src/main/java/com/naveen/notesappmvvm/View/MainActivity.kt
@@ -2,11 +2,29 @@ package com.naveen.notesappmvvm.View
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.naveen.notesappmvvm.NoteApplication
 import com.naveen.notesappmvvm.R
+import com.naveen.notesappmvvm.ViewModel.NoteViewModel
+import com.naveen.notesappmvvm.ViewModel.NoteViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var noteViewModel: NoteViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val viewModelFactory = NoteViewModelFactory((application as NoteApplication).repository)
+
+        noteViewModel = ViewModelProvider(this,viewModelFactory ).get(NoteViewModel::class.java)
+
+        noteViewModel.myAllNotes.observe(this, Observer { notes ->
+
+            // update UI
+
+        })
     }
 }
---
+++ app/src/main/java/com/naveen/notesappmvvm/ViewModel/NoteViewModel.kt
@@ -0,0 +1,36 @@
+package com.naveen.notesappmvvm.ViewModel
+
+import androidx.lifecycle.*
+import com.naveen.notesappmvvm.Model.Note
+import com.naveen.notesappmvvm.Repository.NoteRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class NoteViewModel(private val repository: NoteRepository) : ViewModel() {
+
+    val myAllNotes : LiveData<List<Note>> = repository.myAllNotes.asLiveData()
+
+    fun insert(note: Note) = viewModelScope.launch(Dispatchers.IO) {
+        repository.insert(note)
+    }
+    fun update(note: Note) = viewModelScope.launch(Dispatchers.IO) {
+        repository.update(note)
+    }
+    fun delete(note: Note) = viewModelScope.launch(Dispatchers.IO) {
+        repository.delete(note)
+    }
+    fun deleteAllNotes(note: Note) = viewModelScope.launch(Dispatchers.IO) {
+        repository.deleteAllNotes()
+    }
+
+}
+
+class NoteViewModelFactory(private var repository: NoteRepository) : ViewModelProvider.Factory{
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if(modelClass.isAssignableFrom(NoteViewModel::class.java)){
+            return NoteViewModel(repository) as T
+        } else {
+            throw IllegalArgumentException("unknown view Model")
+        }
+    }
+}
