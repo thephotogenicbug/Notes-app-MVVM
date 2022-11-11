5aabd513b652b72c5d4d8964597d3914783331e8
thephotogenicbug <navinkumarachari@gmail.com>
2022-11-11 18:22:57 +0530

    Add New Note To DB 2/2

--- app/src/main/java/com/naveen/notesappmvvm/NoteAddActivity.java
+++
@@ -1,6 +0,0 @@
-package com.naveen.notesappmvvm;
-
-import android.app.Activity;
-
-public class NoteAddActivity extends Activity {
-}
--- app/src/main/java/com/naveen/notesappmvvm/View/MainActivity.kt
+++ app/src/main/java/com/naveen/notesappmvvm/View/MainActivity.kt
@@ -6,11 +6,15 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.naveen.notesappmvvm.Adapters.NoteAdapter
+import com.naveen.notesappmvvm.Model.Note
 import com.naveen.notesappmvvm.NoteApplication
 import com.naveen.notesappmvvm.R
 import com.naveen.notesappmvvm.ViewModel.NoteViewModel
@@ -21,6 +25,8 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var noteViewModel: NoteViewModel
 
+    lateinit var addActivityResultLauncher: ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -30,6 +36,9 @@ class MainActivity : AppCompatActivity() {
         val noteAdapter = NoteAdapter()
         recyclerView.adapter = noteAdapter
 
+        //register activity launcher
+        registerActivityResultLauncher()
+
         val viewModelFactory = NoteViewModelFactory((application as NoteApplication).repository)
 
         noteViewModel = ViewModelProvider(this,viewModelFactory ).get(NoteViewModel::class.java)
@@ -38,6 +47,24 @@ class MainActivity : AppCompatActivity() {
 
             // update UI
             noteAdapter.setNote(notes)
+
+        })
+    }
+
+
+    fun registerActivityResultLauncher(){
+        addActivityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult(),
+            ActivityResultCallback { resultAddNote ->
+
+                val resultCode = resultAddNote.resultCode
+                val data = resultAddNote.data
+
+                if(resultCode == RESULT_OK && data != null){
+                    val noteTitle : String = data.getStringExtra("title").toString()
+                    val noteDescription : String = data.getStringExtra("description").toString()
+                    val note = Note(noteTitle,noteDescription)
+                    noteViewModel.insert(note)
+                }
 
         })
     }
@@ -53,7 +80,7 @@ class MainActivity : AppCompatActivity() {
 
             R.id.item_add_note -> {
                 val intent = Intent(this, NoteAddActivity::class.java)
-                startActivity(intent)
+               addActivityResultLauncher.launch(intent)
 
             }
             R.id.item_delete_all_notes -> Toast.makeText(applicationContext, "Delete Icon was clicked", Toast.LENGTH_SHORT).show()
